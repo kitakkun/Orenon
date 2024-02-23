27c4d29e28f5add790abdc5463e5a81c2d0fb254
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-02-23 17:07:32 +0900

    move lexer implementation to orenon-compiler module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
 
 rootProject.name = "Orenon"
 
+include(":orenon-compiler")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+    }
+}
