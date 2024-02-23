344bd9aa0e080d06e9293b6c7d1d775d7f8eee80
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-02-23 18:50:47 +0900

    generate visitor code

--- orenon-compiler/src/main/kotlin/com/github/kitakkun/orenon/Expr.kt
+++ orenon-compiler/src/main/kotlin/com/github/kitakkun/orenon/Expr.kt
@@ -4,22 +4,42 @@ package com.github.kitakkun.orenon
 import kotlin.Any
 
 public sealed class Expr {
-  public data class Binary(
-    public val left: Expr,
-    public val right: Expr,
-    public val `operator`: Token,
-  ) : Expr()
+    public abstract fun <R> accept(visitor: Expr.Visitor<R>): R
 
-  public data class Grouping(
-    public val expression: Expr,
-  ) : Expr()
+    public data class Binary(
+        public val left: Expr,
+        public val right: Expr,
+        public val `operator`: Token,
+    ) : Expr() {
+        override fun <R> accept(visitor: Expr.Visitor<R>): R = visitor.visitBinaryExpr(this)
+    }
 
-  public data class Literal(
-    public val `value`: Any,
-  ) : Expr()
+    public data class Grouping(
+        public val expression: Expr,
+    ) : Expr() {
+        override fun <R> accept(visitor: Expr.Visitor<R>): R = visitor.visitGroupingExpr(this)
+    }
 
-  public data class Unary(
-    public val `operator`: Token,
-    public val right: Expr,
-  ) : Expr()
+    public data class Literal(
+        public val `value`: Any,
+    ) : Expr() {
+        override fun <R> accept(visitor: Expr.Visitor<R>): R = visitor.visitLiteralExpr(this)
+    }
+
+    public data class Unary(
+        public val `operator`: Token,
+        public val right: Expr,
+    ) : Expr() {
+        override fun <R> accept(visitor: Expr.Visitor<R>): R = visitor.visitUnaryExpr(this)
+    }
+
+    public interface Visitor<R> {
+        public fun visitBinaryExpr(expr: Expr.Binary): R
+
+        public fun visitGroupingExpr(expr: Expr.Grouping): R
+
+        public fun visitLiteralExpr(expr: Expr.Literal): R
+
+        public fun visitUnaryExpr(expr: Expr.Unary): R
+    }
 }
